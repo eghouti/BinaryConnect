869238f6eee30331c339ae76f62919b334e2a782
Ghouthi Boukli Hacene <ghouthi.bouklihacene@imt-atlantique.fr>
2018-02-12 11:52:32 +0100

    Create binaryconnect.py

---
+++ binaryconnect.py
@@ -0,0 +1,55 @@
+import torch.nn as nn
+import numpy
+from torch.autograd import Variable
+class BC():
+    def __init__(self, model):
+        # count the number of Conv2d and Linear
+        count_targets = 0
+        for m in model.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+                count_targets = count_targets + 1
+
+        start_range = 0
+        end_range = count_targets-1
+        self.bin_range = numpy.linspace(start_range,
+                end_range, end_range-start_range+1)\
+                        .astype('int').tolist()
+        self.num_of_params = len(self.bin_range)
+        self.saved_params = []
+        self.target_params = []
+        self.target_modules = []
+        index = -1
+        for m in model.modules():
+            if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+                index = index + 1
+                if index in self.bin_range:
+                    tmp = m.weight.data.clone()
+                    self.saved_params.append(tmp)
+                    self.target_modules.append(m.weight)
+
+    def binarization(self):
+        self.save_params()
+        for index in range(self.num_of_params):
+            self.target_modules[index].data.copy_(self.target_modules[index].data.sign())
+
+    def save_params(self):
+        for index in range(self.num_of_params):
+            self.saved_params[index].copy_(self.target_modules[index].data)
+
+
+    def restore(self):
+        for index in range(self.num_of_params):
+            self.target_modules[index].data.copy_(self.saved_params[index])
+
+    def clip(self):
+        clip_scale=[]
+        m=nn.Hardtanh(-1, 1)
+        for index in range(self.num_of_params):
+            clip_scale.append(m(Variable(self.target_modules[index].data)))
+        for index in range(self.num_of_params):
+            self.target_modules[index].data.copy_(clip_scale[index].data)
+
+
+
+
+
